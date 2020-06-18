f829bc37923bf2e7b3a2ceb003920249b153d18f
jinsunwoo <sun_woo__jin@naver.com>
2020-06-18 17:47:11 +0900

    Worked on activity_monthly_click.xml, hooked LauncherActivity with LoginActivity and LoginActivity with activity_home.xml.

--- app/src/main/java/com/jsunwoo/dietare/activity/LauncherActivity.java
+++ app/src/main/java/com/jsunwoo/dietare/activity/LauncherActivity.java
@@ -2,15 +2,31 @@ package com.jsunwoo.dietare.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.jsunwoo.dietare.R;
 
 public class LauncherActivity extends AppCompatActivity {
+    // 책장 (하드디스크, ssd). 책상이 메모리(ram). 책을 보고 레포트 작성 책을 찾으러 가는 등 하나 하나 다 쓰레드.
+    // 레포트 쓰는 큰 쓰레드 안에 (책 가져오는 쓰레드 => 책 읽는 쓰레드 => etc).
+    // 쓰레드 작업 리스트를 handling(이거 했다 저거했다 하는 거) 하는게 handler.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        // handler 는 thread 를 handling. thread 는 cpu 가 처리하는 최소 단위. (작업단위).
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent moveIntent = new Intent();
+                moveIntent.setClass(LauncherActivity.this, LoginActivity.class);
+//                Intent moveIntent = new Intent(LauncherActivity.this, LoginActivity.class);
+                startActivity(moveIntent);
+//                startActivity(new Intent(LauncherActivity.this, LoginActivity.class));
+            }
+        }, 2000);
     }
 }
--- app/src/main/java/com/jsunwoo/dietare/activity/LoginActivity.java
+++ app/src/main/java/com/jsunwoo/dietare/activity/LoginActivity.java
@@ -2,9 +2,17 @@ package com.jsunwoo.dietare.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jsunwoo.dietare.R;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +20,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final EditText id = findViewById(R.id.editText_id);
+        final EditText pw = findViewById(R.id.editText_pw);
+        TextView findID = findViewById(R.id.findID);
+        TextView findPW = findViewById(R.id.findPW);
+        Button logIn = findViewById(R.id.button_login);
+        Button signUp = findViewById(R.id.button_signUp);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String arbiID = "abc";
+                String arbiPW = "1234";
+
+                if (!id.getText().toString().equals(arbiID)){
+                    Toast.makeText(LoginActivity.this, "Wrong ID",Toast.LENGTH_SHORT).show();
+                } else if (!pw.getText().toString().equals(arbiPW)){
+                    Toast.makeText(LoginActivity.this, "Wrong PW",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent moveIntent = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(moveIntent);
+                }
+
+            }
+        });
     }
 }
